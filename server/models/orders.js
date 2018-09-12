ff6d198b8854baf48b0c0740bf02454f1af6147c
ElohorFaith <pricelesselohor@gmail.com>
2018-09-12 08:49:32 +0100

    initial commit

---
+++ server/models/orders.js
@@ -0,0 +1,34 @@
+const orders = [
+  {
+    id: 1,
+    name: 'Egusi Soup',
+    price: 'N1000',
+    status: 'Pending',
+    Date: new Date(),
+  },
+
+  {
+    id: 2,
+    name: 'Banga Soup',
+    price: 'N1500',
+    status: 'Accepted',
+    Date: new Date(),
+  },
+
+  {
+    id: 3,
+    name: 'Jollof Rice',
+    price: 'N1000',
+    status: 'Pending',
+    Date: new Date(),
+  },
+
+  {
+    id: 4,
+    name: 'White Soup',
+    price: 'N1300',
+    status: 'Pending',
+    Date: new Date(),
+  },
+];
+export default orders;
